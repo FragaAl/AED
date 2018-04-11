adf3b7dc86220020299afbcbca13e20dbbb10d03
Fralex <alexis.fraga@outlook.com>
2018-04-11 00:21:24 -0300

    Add files via upload

---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,25 @@
+//TP 1 - Adicion
+//adicion de dos enteros ingresados
+//Fraga Alexis
+//20181004
+
+#include <iostream>
+
+main()
+{
+int a,b,suma;
+
+std::cout<<"Ingrese numero a \n" ;
+std::cin>>a;
+std::cout<<"Ingrese numero b \n" ;
+std::cin>>b;
+suma=a+b;
+std::cout<<"La suma de "<<a<<"+"<<b<<" es:"<<suma;
+
+return 0;
+}
+
+
+
+
+
